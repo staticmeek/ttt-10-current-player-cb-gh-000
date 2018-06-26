e9cdadc41ce5a5f53f1751b9b23e02a9bd786c32
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-26 18:14:57 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,13 @@
 def turn_count (board)
   board [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  counter = 0
   board.each do |player|
     counter += 1
   end
+end
+
+def current_player(board)
+  if turn_count(board).counts % 2 == 0
+
+
 end
