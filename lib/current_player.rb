154099b4c26012e73dc10b89f810e91fb4389eac
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-26 18:19:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,7 @@ def turn_count (board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   counter = 0
   board.each do |counter|
+    puts "This is Turn Number: #{counter}"
     counter += 1
   end
 end
